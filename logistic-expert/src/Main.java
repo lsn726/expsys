a966c59e16d9183a3255f8a78aacb57305fe91da
lsn726 <lsn726work@gmail.com>
2014-04-09 16:51:01 +0800

    1

--- logistic-expert/src/Main.java
+++ logistic-expert/src/Main.java
@@ -5,8 +5,8 @@ import com.logsys.demand.DemandProcess;
 public class Main {
 
 	public static void main(String[] args) {
-		//DemandProcess.importDemandFromExcel("e:\\VolvoDemand.xlsx",null);	//导入新需求
-		//DemandProcess.exportDemandToExcel("e:\\Export20140404.xlsx", null, new Date(), null);	//导出需求
+		//DemandProcess.importDemandFromExcel("e:\\Volvo.xlsx",null);			//导入新需求
+		//DemandProcess.exportDemandToExcel("e:\\Export20140409.xlsx", null, new Date(), null);	//导出需求
 		//BOMProcess.uploadBOMFromExcel_SAPCS12("e:\\CS12_22271372.xlsx");		//上传BOM
 		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\pp.xlsx", null, new Date("2014/4/27"),true));	//上传从下周一开始的计划
 		//new MrpReportForExcel().generate("123");								//生成MRP报告
