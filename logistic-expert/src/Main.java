cde88f705216dc6733f9acce26d803ecab8890f8
lsn726 <lsn726work@gmail.com>
2014-04-25 16:45:03 +0800

    1

--- logistic-expert/src/Main.java
+++ logistic-expert/src/Main.java
@@ -1,7 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
-import com.logsys.util.Location;
+import com.logsys.prodplan.ProdplanProcess;
 
 public class Main {
 
@@ -9,7 +8,7 @@ public class Main {
 		//DemandProcess.importDemandFromExcel("e:\\DemandCQAC.xlsx",null);		//导入新需求
 		//DemandProcess.exportDemandToExcel("e:\\Export20140415D.xlsx", null, new Date(), null);	//导出需求
 		//BOMProcess.uploadBOMFromExcel_SAPCS12("e:\\CS12_22271372.xlsx");		//上传BOM
-		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\PP.xlsx", new Date("2014/4/21"), new Date("2014/5/4"),true));	//上传从下周一开始的计划
+		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\PP.xlsx", new Date("2014/4/28"), new Date("2014/5/4"),true));	//上传从下周一开始的计划
 		//new MrpReportForExcel().generate("123");								//生成MRP报告
 		//ProductionProcess.extractOutputDataFromPdExcelFileToDB("e:\\PC.xls", -1);	//将生产日报导入数据库。《《将导入所有数据》》
 		//List list=ProductionDataReaderDB.getProductionDataFromDB(ProdLine.DAMPER_FA_FA2, new DateInterval());
