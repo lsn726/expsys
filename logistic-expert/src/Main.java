684d049a4a2f6765a9779cb90eb9d9c14c7cb3a5
lsn726 <lsn726work@gmail.com>
2014-04-28 16:46:13 +0800

    1

--- logistic-expert/src/Main.java
+++ logistic-expert/src/Main.java
@@ -1,22 +1,17 @@
-import java.io.FileOutputStream;
+import java.util.Date;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import com.logsys.report.MrpReportForExcel;
-import com.logsys.util.Location;
-import com.logsys.util.Matrixable;
+import com.logsys.demand.DemandProcess;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		//DemandProcess.importDemandFromExcel("e:\\DemandCQAC.xlsx",null);		//导入新需求
-		//DemandProcess.exportDemandToExcel("e:\\Export20140415D.xlsx", null, new Date(), null);	//导出需求
+		//DemandProcess.importDemandFromExcel("e:\\Demand.xlsx",null);		//导入新需求
+		//DemandProcess.exportDemandToExcel("e:\\ExportDemand.xlsx", null, new Date(), null);	//导出需求
 		//BOMProcess.uploadBOMFromExcel_SAPCS12("e:\\CS12_22271372.xlsx");		//上传BOM
 		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\PP.xlsx", new Date("2014/4/28"), new Date("2014/5/4"),true));	//上传从下周一开始的计划
 		//ProductionProcess.extractOutputDataFromPdExcelFileToDB("e:\\PC.xls", -1);	//将生产日报导入数据库。《《将导入所有数据》》
+		/**
 		MrpReportForExcel mrpreport=new MrpReportForExcel();
 		Matrixable demmatrix=mrpreport.getDemandMatrix(60);
 		
@@ -30,6 +25,7 @@ public class Main {
 		} catch(Throwable ex) {
 			ex.printStackTrace();
 		}
+		*/
 	}
 
 }
