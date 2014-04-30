a97ebc5bd4798d941f2deedca78fcde7590adcba
lsn726 <lsn726work@gmail.com>
2014-04-30 17:58:06 +0800

    1

--- logistic-expert/src/Main.java
+++ logistic-expert/src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Date;
+
+import com.logsys.prodplan.ProdplanProcess;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		//DemandProcess.importDemandFromExcel("e:\\D20140429.xlsx",null);		//导入新需求
+		//DemandProcess.importDemandFromExcel("e:\\pp.xlsx",null);		//导入新需求
 		//DemandProcess.exportDemandToExcel("e:\\ExportDemand.xlsx", null, new Date(), null);	//导出需求
 		//BOMProcess.uploadBOMFromExcel_SAPCS12("e:\\CS12_22271372.xlsx");		//上传BOM
-		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\PP.xlsx", new Date("2014/4/28"), new Date("2014/5/4"),true));	//上传从下周一开始的计划
+		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\pp.xlsx", new Date("2014/5/3"), new Date("2014/5/11"),true));	//上传从下周一开始的计划
 		//ProductionProcess.extractOutputDataFromPdExcelFileToDB("e:\\PC.xls", -1);	//将生产日报导入数据库。《《将导入所有数据》》
 		//ReportProcess.genDemandMatrixToExcel("e:\\DemandMatrix.xlsx", 25);	//生成需求矩阵报表，矩阵长度(周数)25
 	}
