8d1b9960c4817bf1fc649e231b30425bed32774c
lsn726 <lsn726work@gmail.com>
2014-05-13 16:34:16 +0800

    1

--- logistic-expert/src/Main.java
+++ logistic-expert/src/Main.java
@@ -4,13 +4,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//DemandProcess.importDemandFromExcel("e:\\20140512D.xlsx",null);		//导入新需求
+		//DemandProcess.importDemandFromExcel("e:\\CQACDemand.xlsx",null);		//导入新需求
 		//DemandProcess.exportDemandToExcel("e:\\ExportedDemand.xlsx", null, new Date(), null);	//导出需求
 		//BOMProcess.uploadBOMFromExcel_SAPCS12("e:\\22283596.xlsx");		//上传BOM
 		//System.out.println(ProdplanProcess.importProdplanFromExcel("e:\\PP.xlsx", new Date("2014/5/12"), new Date("2014/5/18"),true));	//上传从下周一开始的计划
 		//ProductionProcess.extractOutputDataFromPdExcelFileToDB("E:\\组装线FA2生产线日报表2014.5.xls", 7);	//将生产日报导入数据库。《《将导入所有数据》》
-		//MatOperDocContentProcess.importSAPMb51ExcelFileIntoDB("e:\\201404.xlsx");	//将SAP的mb51命令倒出的物料操作数据写入数据库
-		
+		//MatOperDocContentProcess.importSAPMb51ExcelFileIntoDB("e:\\201404.xlsx");	//将SAP的mb51命令倒出的物料操作数据写入数据库	
 		
 		//ReportProcess.genDemandMatrixToExcel("e:\\DemandMatrix.xlsx", 25);	//生成需求矩阵报表，矩阵长度(周数)25
 	}
--- logistic-expert/src/com/logsys/production/ProductionUtil.java
+++ logistic-expert/src/com/logsys/production/ProductionUtil.java
@@ -1,10 +1,5 @@
 package com.logsys.production;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
 
 /**
  * 生产工具类
@@ -12,6 +7,6 @@ import org.apache.log4j.Logger;
  */
 public class ProductionUtil {
 
-	private static final Logger logger=Logger.getLogger(ProductionUtil.class);
+	
 
 }
